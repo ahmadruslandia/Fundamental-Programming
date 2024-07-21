536186a76a34116d20cd122a9e1a7f6480622ed7
Ahmad Ruslandia Papua <ruslandiaahmad@gmail.com>
2024-07-21 16:56:31 +0800

    update

---
+++ JavaScript/Contoh Array/Array/Array.js
@@ -0,0 +1,7 @@
+const huruf = [];
+huruf[0] = "a";
+huruf[1] = "b";
+huruf[2] = "c"; 
+document.getElementById("hrf1").innerHTML = huruf[0];
+document.getElementById("hrf2").innerHTML = huruf[1];
+document.getElementById("hrf3").innerHTML = huruf[2];
